48f21d8aef960bc23f278832b4ee2080d442c347
Adam Wiggins <adam@heroku.com>
2011-04-28 13:37:14 -0700

    usability improvements for fetch

--- fetch.rb
+++ fetch.rb
@@ -1,4 +1,8 @@
 require 'chronic'
+require 'fileutils'
+
+abort("Set TZ=UTC please") unless ENV['TZ'] == 'UTC'
+abort("No S3_BUCKET specified") unless ENV['S3_BUCKET']
 
 usage = "usage: #{$0} FROM TO\nexample: #{$0} '3 hours ago' now"
 from_s = ARGV.shift or abort(usage)
@@ -9,6 +13,18 @@ to = Chronic.parse(to_s) or abort("#{to_s} failed to parse")
 
 puts "Fetching #{from} until #{to} (#{((to - from) / 60).to_i} minutes)"
 
+STDOUT.sync = true
+print "["
+
+$files = []
+
+def cleanup
+  $files.each { |f| FileUtils.rm(f) }
+  puts
+end
+
+trap('INT') { cleanup }
+
 cursor = from
 while cursor < to
   t = cursor
@@ -16,10 +32,29 @@ while cursor < to
   truncated_min = sprintf("%02d", (t.min / 10).to_i * 10)
   timerange = "#{t.year}-#{t.month}-#{t.day}-#{t.hour}:#{truncated_min}:00"
   file = "log-#{timerange}.txt"
-  puts file
 
   url = "http://s3.amazonaws.com/#{ENV['S3_BUCKET']}/#{file}"
   system "curl #{url} -o #{file} --silent --fail"
+  unless $?.success? or $?.exitstatus == 22
+    cleanup
+    exit(1)
+  end
+
+  if File.exists?(file)
+    $files << file
+    print "="
+  else
+    print "_"
+  end
 
   cursor = t + 10*60
+end
+
+if $files.empty?
+  puts "No logs found for that period"
+else
+  puts "]"
+  system "cat #{$files.join(' ')} > log.txt"
+  $files.each { |f| FileUtils.rm(f) }
+  puts "done, result is in log.txt"
 end
